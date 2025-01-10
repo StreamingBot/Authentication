6840fadd2cb0b86e7904f969fd050949daedd14b
Popodil <popodil@bornready.eu>
2025-01-10 03:40:53 +0100

    Initial commit

---
+++ src/test/java/com/streamingbot/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamingbot.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
